4334060ca4c0b473b98fd5944852d1be9663d08c
outsider-kithy <kishida@outsider-design-tech.net>
2024-06-17 17:54:32 +0900

    メディアクエリ反映

--- js/common.js
+++ js/common.js
@@ -1,10 +1,26 @@
+//htmlの内容が全て読み込まれたら実行
 window.addEventListener("DOMContentLoaded", function(){
     //トグルボタン
-    var toggle = document.getElementById('toggle');
-    var gnav = document.getElementById('gnav');
+    let toggle = document.getElementById('toggle');
+    let gnav = document.getElementById('gnav');
 
+    //トグルボタンがクリックされたら実行
     toggle.onclick = function(){
         this.classList.toggle('open');
         gnav.classList.toggle('open');
     }
 });
+
+ //トップへ戻るボタン
+ let totop = document.getElementById("totop");
+ window.addEventListener("scroll", function () {
+     if (window.scrollY > 500) {
+     totop.classList.add("show");
+     }
+ });
+ totop.addEventListener("click", function () {
+     window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+     });
+ });
