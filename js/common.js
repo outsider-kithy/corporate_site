812c808c0f3939c5e9c9ffe7749ab0a251bedd1d
outsider-kithy <kishida@outsider-design-tech.net>
2024-06-17 21:05:18 +0900

    jsブランチ修正

--- js/common.js
+++ js/common.js
@@ -9,6 +9,33 @@ window.addEventListener("DOMContentLoaded", function(){
         this.classList.toggle('open');
         gnav.classList.toggle('open');
     }
+
+    //スライダー
+    $('.slider').slick({
+        autoplay:true,
+        infinite:true,
+        fade:true,
+        autoplayspeed:1600,
+        speed:1000,
+        dots:true,
+        arrows:false
+    });
+
+    //スクロールアニメーション
+    let targets = document.querySelectorAll('.scrollIn');
+    let offset = 100;
+
+    window.addEventListener('scroll',function(){
+        let scroll = window.scrollY;
+        let height = window.innerHeight;
+
+        for(let target of targets){
+            let pos = target.getBoundingClientRect().top+scroll;
+            if(scroll > pos - height + offset){
+                target.classList.add('scrollOff');
+            }
+        }
+    });
 });
 
  //トップへ戻るボタン
